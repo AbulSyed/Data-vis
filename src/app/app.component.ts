e77f871fa97a2cee7b6e16c73297747660bdacc1
Abul Syed <syed1234@hotmail.co.uk>
2021-12-15 00:17:39 +0000

    vis1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import embed from 'vega-embed';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'data-vis';
+
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    this.embedGraph();
+  }
+
+  async embedGraph(): Promise<void> {
+    const spec = "https://abulsyed.github.io/stats/test.json";
+    const result = await embed("#vis1", spec);
+    console.log(result.view);
+  }
 }
